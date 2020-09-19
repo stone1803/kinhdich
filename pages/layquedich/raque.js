b607c33ad70216e253344d0e4aafd1153fd968d2
Phuc le <jangmi119085@gmail.com>
2020-09-19 13:49:16 +0700

    quehoaupdata

--- pages/layquedich/raque.js
+++ pages/layquedich/raque.js
@@ -10,18 +10,19 @@ export default function Raque({ data, props }) {
     messagingSenderId: "88913793943",
     appId: "1:88913793943:web:46f043cd5b104430a92ad2",
     measurementId: "G-7R6EHMTQNZ",
-};
-try {
-  firebase.initializeApp(firebaseConfig);
-} catch(err){
-  if (!/already exists/.test(err.message)) {
-    console.error('Firebase initialization error', err.stack)}
-}
-const fire = firebase;
+  };
+  try {
+    firebase.initializeApp(firebaseConfig);
+  } catch (err) {
+    if (!/already exists/.test(err.message)) {
+      console.error("Firebase initialization error", err.stack);
+    }
+  }
+  const fire = firebase;
   useEffect(() => {
     tinhQue();
   }, []);
-  const [dong,setQueDong]= useState("")
+  const [dong, setQueDong] = useState("");
   const [tenQue, SetTenQue] = useState([]);
   const [quethuong, setquethuong] = useState(null);
   const [queha, setqueha] = useState(null);
@@ -34,7 +35,7 @@ const fire = firebase;
     let quethuong = ((data.nam + data.thang + data.ngay) % 8).toString();
     let queha = ((data.ngay + data.thang + 1 + data.gio) % 8).toString();
     let quedong = (data.ngay + data.thang + data.gio) % 6;
-    setQueDong(quedong)
+    setQueDong(quedong);
     setqueha(queha);
     setquethuong(quethuong);
     let phuc = `${quethuong}${queha}`;
@@ -53,57 +54,52 @@ const fire = firebase;
           content["tenQue"] = result.data().tenQue;
           content["tenQueHo"] = result.data().tenQueHo;
           content["hinhAnhQue"] = result.data().hinhAnhQue;
+          content["hinhAnhQueHo"] = result.data().hinhAnhQueHo;
 
           content["ynghia"] = result.data().ynghia;
-          console.log(result.data().hinhAnhQue)
+          content["ynghiaHo"] = result.data().ynghiaHo;
+
+          console.log(result.data().hinhAnhQue);
         }
 
         setKetQua({
           tenque: content.tenQue,
           tenqueho: content.tenQueHo,
           ynghia: content.ynghia,
-          hinhAnhQue : content.hinhAnhQue
+          hinhAnhQue: content.hinhAnhQue,
+          hinhAnhQueHo: content.hinhAnhQueHo,
+          ynghiaHo: content.ynghiaHo,
+
         });
       });
   };
-console.log(ketqua)
+  console.log(ketqua);
   return (
- 
-      <div class="flex flex-wrap container">
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 bg-gray-500">
-    <h1> QUẺ CHÍNH - Động Hào : {dong} </h1>
-  <p>{ketqua.tenque}</p>
-      <img 
-            class="px-4"
+    <div class="flex flex-wrap container">
+      <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 bg-gray-500">
+        <h1> QUẺ CHÍNH - Động Hào : {dong} </h1>
+        <p>{ketqua.tenque}</p>
+        <img class="px-4" src={ketqua.hinhAnhQue} />
 
-      src={ketqua.hinhAnhQue}/>
+        <p>{ketqua.ynghia}</p>
+      </div>
+      <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 bg-gray-500">
+        <h1> QUẺ HỖ </h1>
 
-      <p>{ketqua.ynghia}</p>
-  </div>
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 bg-gray-500">
-  <h1> QUẺ HỖ </h1>
+        <p>{ketqua.tenqueho}</p>
+        <img class="px-4" src={ketqua.hinhAnhQueHo} />
 
-  <p>{ketqua.tenQueHo}</p>
-      <img 
-            class="px-4"
+        <p>{ketqua.ynghiaHo}</p>
+      </div>
+      <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 bg-gray-500">
+        <h1> QUẺ BIẾN </h1>
 
-      src={ketqua.hinhAnhQueHo}/>
+        <p>{ketqua.tenque}</p>
+        <img class="px-4" src={ketqua.hinhAnhQue} />
 
-      <p>{ketqua.ynghiaHo}</p>
-
-  </div>
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 bg-gray-500">
-  <h1> QUẺ BIẾN </h1>
-
-  <p>{ketqua.tenque}</p>
-      < img 
-      class="px-4"
-      src={ketqua.hinhAnhQue}/>
-
-      <p>{ketqua.ynghia}</p>
-  </div>
-
-</div>
+        <p>{ketqua.ynghia}</p>
+      </div>
+    </div>
   );
 }
 export const getServerSideProps = async ({ query }) => {
